ffc0df6f1c86210e0a1c46f01df5c90c602f998a
Charles Sotonye <sotycharlex@gmail.com>
2021-07-02 12:47:10 +0100

    Update veiws.go

--- Chapter_six/views/veiws.go
+++ Chapter_six/views/veiws.go
@@ -1,18 +1,45 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
 
-func NewView(files ...string) *View {
-	files = append(files, "layouts/footer.gohtml")
+var (
+	LayoutDir   string = "layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+func NewView(layout string, files ...string) *View {
+	files = append(files, layoutFiles()...)
+
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
+
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	
+	return files
+
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
+}
+
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
